5c181908c5f88416ef7fdc922919b80c9a5957b4
swaroopanand <swaroopanand695@gmail.com>
2022-04-12 11:29:33 +0530

    updated whichkey

--- lua/user/whichkey.lua
+++ lua/user/whichkey.lua
@@ -86,6 +86,7 @@ local mappings = {
   },
   ["e"] = { "<cmd>NvimTreeToggle<cr>", "Explorer" },
   ["w"] = { "<cmd>w!<CR>", "Save" },
+  ["i"] = {"<cmd>wq!<CR>", "Save+Quit"},
   ["q"] = { "<cmd>q!<CR>", "Quit" },
   ["c"] = { "<cmd>Bdelete!<CR>", "Close Buffer" },
   ["h"] = { "<cmd>nohlsearch<CR>", "No Highlight" },
